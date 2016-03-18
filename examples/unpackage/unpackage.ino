00fc0e38d153c1a449bcb038b4473a40a1790130
joey <astainsong@gmail.com>
2016-03-18 18:42:02 +0800

    add all

---
+++ examples/unpackage/unpackage.ino
@@ -0,0 +1,156 @@
+/****************************************************************************************************
+* File Name          : unpackage
+* Author             : Joey Song
+* Version            : V1.0
+* Date               : 29 Aug, 2015
+* Modified Date      : 29 Aug, 2015
+* Description        : This documents is for unpackage a iphone box, only suitable for uArm metal
+* Copyright(C) 2015 uArm Team. All right reserved.
+*****************************************************************************************************/
+
+
+#include <EEPROM.h>
+#include <Wire.h>
+#include <uarm_library.h>
+#include <Servo.h>
+
+// headers need to be loaded first
+
+
+int valuel;
+int buttonState;
+int stopperPin = 2;                 // stopper is mounted under servo 4
+
+int left_pos = 3;                   // left side of the center, move 3 cm left 
+int right_pos = -3;                 // right side of the center, move 3 cm right 
+
+int centerOfBox_x = 0;              // assume x of center of the box is 0
+int centerOfBox_y = -18;            // assume y of center of the box is -18
+int centerOfBox_z_case = 22;        // assume z of center of the box case is 22
+int centerOfBox_z_others = 18;      // assume x of center of the other things is 18
+int rotationDegreeLeft = 115;       // assume rotate object to left need 115 degree rotation
+int rotationDegreeRight = 68;       // assume rotate object to right need 68 degree rotation
+
+void setup() {
+      
+      Wire.begin();        // join i2c bus (address optional for master)
+      Serial.begin(9600);  // start serial port at 9600 bps
+//      uarm.init();
+      pinMode(stopperPin,INPUT);
+      
+}
+
+void loop(){
+  
+  if(Serial.available()>0)
+  {
+    
+    char readSerial = Serial.read();
+    Serial.println(readSerial);
+    //read what you input to the console
+
+    
+    // first input a to the console to make sure uArm is located above the center of iphone box 
+      if(readSerial == 'a')
+    {
+       uarm.moveTo(centerOfBox_x, centerOfBox_y, centerOfBox_z_case);
+    }
+
+    // then you can input b to execute main functions
+    if(readSerial == 'b')
+    {
+      mainMove(centerOfBox_x, centerOfBox_y, centerOfBox_z_case, 0, -30, 19, 0);
+        // Step 1: object 1 - big case 
+        // initial position x,y,z = 0, -18, 22 (above the center of box)
+        // destination position x = 0, y = -30, z = 19;
+        // rotation angle = 0  means the object doesn't need to be rotated
+
+      mainMove(centerOfBox_x, centerOfBox_y, centerOfBox_z_case, 0, -30, 19, 0);
+        // Step 2: object 2 - iphone! 
+        // initial position x,y,z = 0, -18, 18 (above the center of box)
+        // destination position x = 0, y = -30, z = 19;
+        // rotation angle = 0  means the object doesn't need to be rotated
+
+      mainMove(right_pos, centerOfBox_y, centerOfBox_z_others, 14, -26, 18, rotationDegreeLeft);
+        // Step 3: object 3 - iphone holder case
+        // initial position x,y,z = 3, -18, 18 (left of the center of box, because there is a hold in the case)
+        // destination position x = 14, y = -29, z = 18;
+        // rotation angle != 0  means the object need to be rotated 
+      
+      mainMove(left_pos, centerOfBox_y, centerOfBox_z_others, -14, -26, 18, rotationDegreeRight);
+        // Step 4: object 4 - user-manual 
+        // initial position x,y,z = 0, -18, 18 (above the center of box)
+        // destination position x = -14, y = -29, z = 18;
+        // rotation angle != 0  means the object need to be rotated 
+      
+      mainMove(left_pos, centerOfBox_y-1, centerOfBox_z_others,10, -11, 17, 45);
+        // Step 5: object 5 - earphone case
+        // initial position x,y,z = 0, -18, 18 (above the center of box)
+        // destination position x = 10, y = -11, z = 18;
+        // rotation angle = 45  means the object need to be rotate 45 degree due to the rotation of base servo    
+           
+      mainMove(right_pos-2, centerOfBox_y-1, centerOfBox_z_others, -10, -11, 17, 45);
+        // Step 6: object 6 - charger
+        // initial position x,y,z = 0, -18, 18 (above the center of box)
+        // destination position x = -10, y = -11, z = 18;
+        // rotation angle = 45  means the object need to be rotate 45 degree due to the rotation of base servo  
+
+      uarm.moveTo(centerOfBox_x, centerOfBox_y, centerOfBox_z_case);     // finally, move back to initial position
+      digitalWrite(6,LOW);      // double check to disable the pump
+      digitalWrite(5,LOW);
+   }
+   
+   delay(50);
+  } // end of reading
+}
+
+
+// main move procdure
+void mainMove(double iniX, double iniY, double iniZ, double desX, double desY, double desZ, int rotDeg)
+  // iniX, iniY and iniZ are the initial position of uArm
+  // desX, desY, desZ are the desitination of uArm
+  // rotDeg is the rotation degree of end-effector to rotate the object
+{
+  uarm.moveTo(iniX,iniY,iniZ);          // move the initial position
+  absorbFcn(1,0, iniX, iniY , iniZ);    // move end-effector downwards until stopper hit something, 1 means begin to absorb 
+  uarm.moveTo(0,0,10,1,6);              // move upwards for 10 cm in 6 seconds - slow upwards (relative = 1, means only move in the z axis)
+  uarm.moveTo(desX,desY,desZ,0,2);      // move the destination position
+  absorbFcn(2,rotDeg,desX,desY,desZ);   // move end-effector downwards until stopper hit something, 0 means disable the pump
+  uarm.moveTo(0,0,5,1,4,rotDeg);        // move the upwards in 4 seconds - slow upwards
+  uarm.moveTo(desX,desY,desZ);          // move to destination
+}
+
+
+// to control the pump to absorb stuff
+void absorbFcn(int trigger, int rotDeg, double currentX, double currentY, double currentZ)
+{
+  buttonState = 1;                                // buttonState is the stopper state.
+  while ( buttonState)
+    {
+      buttonState = digitalRead(stopperPin);      // if stopper hit anything, the reading would be LOW
+      if(buttonState == HIGH)                     // while reading is HIGH, keep move downwards for 2 cm
+      {
+        uarm.moveTo(currentX,currentY,currentZ,0,0.5,rotDeg);
+        currentZ = currentZ - 2;    // every time movedowards for 0.2 cm
+        delay(50);
+      }
+      
+    }
+  
+  // after stopper detects the end-effector hit something, pump will begin to work
+  
+  switch (trigger){
+    case 1:                       //absorb
+        digitalWrite(6,HIGH);     // begin to absorb
+        digitalWrite(5,LOW);
+        delay(500);               // wait 500 ms to make sure object is absorbed
+      break;
+    case 2:                       //release
+        digitalWrite(6,LOW);      // begin to release quickly
+        digitalWrite(5,HIGH);     
+      break;
+    default:
+      break;
+  }
+  
+}
---
+++ uarm_library.cpp
@@ -0,0 +1,563 @@
+/******************************************************************************
+* File Name          : uArm_library.cpp
+* Author             : Joey Song  
+* Updated            : Joey Song, Alex Tan, Dave Corboy
+* Email              : joey@ufactory.cc
+* Version            : V1.2 
+* Date               : 12 Dec, 2014
+* Modified Date      : 17 Dec, 2015
+* Description        : 
+* License            : 
+* Copyright(C) 2014 UFactory Team. All right reserved.
+*******************************************************************************/
+
+#include "uarm_library.h"
+
+uArmClass uarm;
+
+uArmClass::uArmClass()
+{
+  g_servo_offset = 0.0;
+}
+
+void uArmClass::alert(byte times, byte runTime, byte stopTime)
+{
+  for(int ct=0; ct < times; ct++)
+  {
+    delay(stopTime);
+    digitalWrite(BUZZER, HIGH);
+    delay(runTime);
+    digitalWrite(BUZZER, LOW);
+  }
+}
+
+/* The code below is written by jerry song */
+
+void uArmClass::writeAngle(byte servo_rot_angle, byte servo_left_angle, byte servo_right_angle, byte servo_hand_rot_angle, byte trigger)
+{
+  attachAll();
+
+  writeServoAngle(SERVO_ROT_NUM,round(servo_rot_angle),false);
+  writeServoAngle(SERVO_LEFT_NUM,round(servo_left_angle),false);
+  writeServoAngle(SERVO_RIGHT_NUM,round(servo_right_angle),false);
+  writeServoAngle(SERVO_HAND_ROT_NUM,round(servo_hand_rot_angle),false);
+
+
+  // refresh logical servo angle cache
+  cur_rot = readAngle(SERVO_ROT_NUM);
+  cur_left = readAngle(SERVO_LEFT_NUM);
+  cur_right = readAngle(SERVO_RIGHT_NUM);
+  cur_hand = readAngle(SERVO_HAND_ROT_NUM);
+}
+
+void uArmClass::writeServoAngle(byte servo_number, double servo_angle, boolean writeWithoffset)
+{
+  attachAll();
+  servo_angle = writeWithoffset ? round(inputToReal(servo_number,servo_angle)): round(servo_angle);
+  servo_angle = constrain(servo_angle,0,180);
+
+  switch(servo_number)
+  {
+    case SERVO_ROT_NUM:       g_servo_rot.write(servo_angle);
+                              break;
+    case SERVO_LEFT_NUM:      g_servo_left.write(servo_angle);
+                              break;
+    case SERVO_RIGHT_NUM:     g_servo_right.write(servo_angle);
+                              break;
+    case SERVO_HAND_ROT_NUM:  g_servo_hand_rot.write(servo_angle);
+                              break;                        
+    default:                  break;
+  }
+}
+
+void uArmClass::writeAngle(double servo_rot_angle, double servo_left_angle, double servo_right_angle, double servo_hand_rot_angle)
+{
+  attachAll();
+  
+  if(servo_left_angle < 10) servo_left_angle = 10;
+  if(servo_left_angle > 120) servo_left_angle = 120;
+  if(servo_right_angle < 10) servo_right_angle = 10;
+  if(servo_right_angle > 110) servo_right_angle = 110;
+
+  if(servo_left_angle + servo_right_angle > 160) 
+  {
+    servo_right_angle = 160 - servo_left_angle;
+    return;
+  }
+  writeServoAngle(SERVO_ROT_NUM,servo_rot_angle,true);
+  writeServoAngle(SERVO_LEFT_NUM,servo_left_angle,true);
+  writeServoAngle(SERVO_RIGHT_NUM,servo_right_angle,true);
+  writeServoAngle(SERVO_HAND_ROT_NUM,servo_hand_rot_angle,true);
+  
+
+  // refresh logical servo angle cache
+  cur_rot = servo_rot_angle;
+  cur_left = servo_left_angle;
+  cur_right = servo_right_angle;
+  cur_hand = servo_hand_rot_angle;
+}
+
+void uArmClass::attachAll()
+{
+  if (!g_servo_rot.attached()) {
+    g_servo_rot.attach(11);
+    cur_rot = readAngle(SERVO_ROT_NUM);
+  }
+  if (!g_servo_left.attached()) {
+    g_servo_left.attach(13);
+    cur_left = readAngle(SERVO_LEFT_NUM);
+  }
+  if (!g_servo_right.attached()) {
+    g_servo_right.attach(12);
+    cur_right = readAngle(SERVO_RIGHT_NUM);
+  }
+  if (!g_servo_hand_rot.attached()) {
+    g_servo_hand_rot.attach(10);
+    cur_hand = readAngle(SERVO_HAND_ROT_NUM);
+  }
+}
+
+void uArmClass::detachAll()
+{
+  g_servo_rot.detach();
+  g_servo_left.detach();
+  g_servo_right.detach();
+  g_servo_hand_rot.detach();
+}
+
+byte uArmClass::inputToReal(byte servo_num, double input_angle)
+{
+  return (byte)round(constrain((input_angle + readServoOffset(servo_num)),0,180));
+}
+
+double uArmClass::readServoOffset(byte servo_num)
+{
+  if ((servo_num == 1)||(servo_num == 2)||(servo_num == 3))
+  {
+    g_servo_offset = (EEPROM.read(LINEAR_START_ADDRESS + (servo_num-1)*2 +1))/10.00;
+
+    if (EEPROM.read(LINEAR_START_ADDRESS + (servo_num-1)*2 ) == 0)
+      {g_servo_offset = - g_servo_offset;}
+
+    return g_servo_offset;
+  } 
+  else if (servo_num == 4)
+    return 0;
+  else {
+      Serial.println(F("Incorrect"));
+  }
+}
+
+void uArmClass::saveDataToRom(double data, int address)
+{
+  int dataWhole;
+
+  byte Byte0;
+  byte Byte1;
+  byte Byte2;
+
+  if(abs(data) < 1) {
+    dataWhole = (int) (data*100);
+  }
+  else{
+    dataWhole = (int) (data*10);
+  }
+
+  if (dataWhole > 0){ 
+    Byte0 = 1;
+  }
+  else{ 
+    Byte0 =0; 
+  }
+
+  dataWhole = abs(dataWhole);
+
+  Byte1 = (( dataWhole >> 0) & 0xFF);
+  Byte2 = (( dataWhole >> 8) & 0xFF);
+
+  EEPROM.write( address, Byte0);
+  EEPROM.write( address + 1, Byte1);
+  EEPROM.write( address + 2, Byte2);
+}
+
+double uArmClass::readToAngle(double input_analog, byte servo_num, byte trigger)
+{
+  int address = OFFSET_START_ADDRESS +(servo_num-1)*6;
+
+  double data_a = (EEPROM.read(address+1)+EEPROM.read(address+2)*256)/10.0;
+  if (EEPROM.read(address)==0)
+    {data_a = -data_a;}
+
+  double data_b = (EEPROM.read(address+4)+EEPROM.read(address+5)*256)/100.0;
+  if (EEPROM.read(address+3)==0)
+    {data_b = -data_b;}
+
+  if (trigger == 0){
+    return (data_a + data_b*input_analog) - readServoOffset(servo_num);
+  }
+  if (trigger == 1){
+    return (data_a + data_b*input_analog);
+  }
+}
+
+double uArmClass::readAngle(byte servo_num)
+{
+  return readAngle(servo_num, 0);
+}
+
+double uArmClass::readAngle(byte servo_num, byte trigger)
+{
+  switch (servo_num)
+  {
+    case SERVO_ROT_NUM:
+      return readToAngle(analogRead(SERVO_ROT_ANALOG_PIN),SERVO_ROT_NUM,trigger);
+      break;
+
+    case SERVO_LEFT_NUM:
+      return readToAngle(analogRead(SERVO_LEFT_ANALOG_PIN),SERVO_LEFT_NUM,trigger);
+      break;
+
+    case SERVO_RIGHT_NUM:
+      return readToAngle(analogRead(SERVO_RIGHT_ANALOG_PIN),SERVO_RIGHT_NUM,trigger);
+      break;
+
+    case SERVO_HAND_ROT_NUM:
+      return readToAngle(analogRead(SERVO_HAND_ROT_ANALOG_PIN),SERVO_HAND_ROT_NUM,trigger);
+      break;
+
+    default:
+      break;
+
+  }
+}
+
+/*Action control */
+
+void uArmClass::calAngles(double x, double y, double z)
+{
+   if (z > (MATH_L1 + MATH_L3 + TopOffset))
+    {
+        z = MATH_L1 + MATH_L3 + TopOffset;
+    }
+
+    if (z < (MATH_L1 - MATH_L4 + BottomOffset))
+    {
+        z = MATH_L1 - MATH_L4 + BottomOffset;
+    }
+
+  double x_in = 0.0;
+  double y_in = 0.0;
+  double z_in = 0.0;
+  double right_all = 0.0;
+  double right_all_2 = 0.0;
+  double sqrt_z_x = 0.0;
+  double sqrt_z_y = 0.0;
+  double phi = 0.0;
+
+  y_in = (-y-MATH_L2)/MATH_L3;
+  z_in = (z - MATH_L1) / MATH_L3;
+  right_all = (1 - y_in*y_in - z_in*z_in - MATH_L43*MATH_L43) / (2 * MATH_L43);
+  sqrt_z_y = sqrt(z_in*z_in + y_in*y_in);
+
+  if (x == 0)
+  {
+    // Calculate value of theta 1
+    g_theta_1 = 90;
+
+    // Calculate value of theta 3
+    if (z_in == 0) {
+      phi = 90;
+    }
+
+    else {
+    phi = atan(-y_in / z_in)*MATH_TRANS;
+    }
+
+    if (phi > 0) phi = phi - 180;
+
+    g_theta_3 = asin(right_all / sqrt_z_y)*MATH_TRANS - phi;
+    if(g_theta_3<0)
+      {
+        g_theta_3 = 0;
+      }
+
+    // Calculate value of theta 2
+    g_theta_2 = asin((z + MATH_L4*sin(g_theta_3 / MATH_TRANS) - MATH_L1) / MATH_L3)*MATH_TRANS;
+  }
+  else
+  {
+    // Calculate value of theta 1
+
+    g_theta_1 = atan(y / x)*MATH_TRANS;
+
+    if (y / x > 0) {
+      g_theta_1 = g_theta_1;
+    }
+    if (y / x < 0) {
+      g_theta_1 = g_theta_1 + 180;
+    }
+    if (y == 0) {
+      if (x > 0) g_theta_1 = 180;
+      else g_theta_1 = 0;       
+    }
+
+    // Calculate value of theta 3
+
+    x_in = (-x / cos(g_theta_1 / MATH_TRANS) - MATH_L2) / MATH_L3;
+
+    if (z_in == 0){ phi = 90; }
+
+    else{ phi = atan(-x_in / z_in)*MATH_TRANS; }
+
+    if (phi > 0) {phi = phi - 180;}  
+
+    sqrt_z_x = sqrt(z_in*z_in + x_in*x_in);
+
+    right_all_2 = -1 * (z_in*z_in + x_in*x_in + MATH_L43*MATH_L43 - 1) / (2 * MATH_L43);
+    g_theta_3 = asin(right_all_2 / sqrt_z_x)*MATH_TRANS;
+    g_theta_3 = g_theta_3 - phi;
+
+    if (g_theta_3 <0 ) {
+      g_theta_3 = 0;
+    }
+
+    // Calculate value of theta 2
+    g_theta_2 = asin(z_in + MATH_L43*sin(abs(g_theta_3 / MATH_TRANS)))*MATH_TRANS;
+  }
+
+  g_theta_1 = abs(g_theta_1);
+  g_theta_2 = abs(g_theta_2);
+
+  if (g_theta_3 < 0 ){}
+  else{
+    if ((calYonly(g_theta_1,g_theta_2, g_theta_3)>y+0.1)||(calYonly(g_theta_1,g_theta_2, g_theta_3)<y-0.1))
+    {
+      g_theta_2 = 180 - g_theta_2;
+    }  
+  }
+
+  if(isnan(g_theta_1)||isinf(g_theta_1))
+    {g_theta_1 = uarm.readAngle(1);}
+  if(isnan(g_theta_2)||isinf(g_theta_2))
+    {g_theta_2 = uarm.readAngle(2);}
+  if(isnan(g_theta_3)||isinf(g_theta_3)||(g_theta_3<0))
+    {g_theta_3 = uarm.readAngle(3);}
+}
+
+void uArmClass::calXYZ(double theta_1, double theta_2, double theta_3)
+{
+  // g_l3_1 = MATH_L3 * cos(theta_2 / MATH_TRANS);
+  // g_l4_1 = MATH_L4*cos(theta_3 / MATH_TRANS);
+  double l5 = (MATH_L2 + MATH_L3*cos(theta_2 / MATH_TRANS) + MATH_L4*cos(theta_3 / MATH_TRANS));
+
+  g_cal_x = -cos(abs(theta_1 / MATH_TRANS))*l5;
+  g_cal_y = -sin(abs(theta_1 / MATH_TRANS))*l5;
+  g_cal_z = MATH_L1 + MATH_L3*sin(abs(theta_2 / MATH_TRANS)) - MATH_L4*sin(abs(theta_3 / MATH_TRANS));
+}
+
+void uArmClass::calXYZ()
+{
+  calXYZ(
+  uarm.readToAngle(analogRead(SERVO_ROT_ANALOG_PIN),SERVO_ROT_NUM,ABSOLUTE),
+  uarm.readToAngle(analogRead(SERVO_LEFT_ANALOG_PIN),SERVO_LEFT_NUM,ABSOLUTE),
+  uarm.readToAngle(analogRead(SERVO_RIGHT_ANALOG_PIN),SERVO_RIGHT_NUM,ABSOLUTE));
+}
+
+void uArmClass::interpolate(double start_val, double end_val, double (&interp_vals)[INTERP_INTVLS], byte ease_type) {
+  double delta = end_val - start_val;
+  for (byte f = 0; f < INTERP_INTVLS; f++) {
+    switch (ease_type) {
+      case INTERP_LINEAR :
+        interp_vals[f] = delta * f / INTERP_INTVLS + start_val;
+        break;
+      case INTERP_EASE_INOUT :
+        {
+          float t = f / (INTERP_INTVLS / 2.0);
+          if (t < 1) {
+            interp_vals[f] = delta / 2 * t * t + start_val;
+          } else {
+            t--;
+            interp_vals[f] = -delta / 2 * (t * (t - 2) - 1) + start_val;
+          }
+        }
+        break;
+      case INTERP_EASE_IN :
+        {
+          float t = (float)f / INTERP_INTVLS;
+          interp_vals[f] = delta * t * t + start_val;
+        }
+        break;
+      case INTERP_EASE_OUT :
+        {
+          float t = (float)f / INTERP_INTVLS;
+          interp_vals[f] = -delta * t * (t - 2) + start_val;
+        }
+        break;
+      case INTERP_EASE_INOUT_CUBIC :  // this is a compact version of Joey's original cubic ease-in/out
+        {
+          float t = (float)f / INTERP_INTVLS;
+          interp_vals[f] = start_val + (3 * delta) * (t * t) + (-2 * delta) * (t * t * t);
+        }
+        break;
+    }
+  }
+}
+
+void uArmClass::moveToOpts(double x, double y, double z, double hand_angle, byte relative_flags, double time, byte path_type, byte ease_type) {
+
+  attachAll();
+
+  // find current position using cached servo values
+  double current_x;
+  double current_y;
+  double current_z;
+  getCalXYZ(cur_rot, cur_left, cur_right, current_x, current_y, current_z);
+
+  // deal with relative xyz positioning
+  byte posn_relative = (relative_flags & F_POSN_RELATIVE) ? 1 : 0;
+  x = current_x * posn_relative + x;
+  y = current_y * posn_relative + y;
+  z = current_z * posn_relative + z;
+
+  // find target angles
+  double tgt_rot;
+  double tgt_left;
+  double tgt_right;
+  uarm.getCalAngles(x, y, z, tgt_rot, tgt_left, tgt_right);
+
+  // deal with relative hand orientation
+  if (relative_flags & F_HAND_RELATIVE) {
+    hand_angle += cur_hand;                                     // rotates a relative amount, ignoring base rotation
+  } else if (relative_flags & F_HAND_ROT_REL) {
+    hand_angle = hand_angle + cur_hand + (tgt_rot - cur_rot);   // rotates relative to base servo, 0 value keeps an object aligned through movement
+  }
+  
+  if (time > 0) {
+    if (path_type == PATH_ANGLES) {
+      // we will calculate angle value targets
+      double rot_array[INTERP_INTVLS];
+      double left_array[INTERP_INTVLS];
+      double right_array[INTERP_INTVLS];
+      double hand_array[INTERP_INTVLS];
+
+      interpolate(cur_rot, tgt_rot, rot_array, ease_type);
+      interpolate(cur_left, tgt_left, left_array, ease_type);
+      interpolate(cur_right, tgt_right, right_array, ease_type);
+      interpolate(cur_hand, hand_angle, hand_array, ease_type);
+
+      for (byte i = 0; i < INTERP_INTVLS; i++)
+      {
+        writeAngle(rot_array[i], left_array[i], right_array[i], hand_array[i]);
+        delay(time * 1000 / INTERP_INTVLS);
+      }
+    } else if (path_type == PATH_LINEAR) {
+      // we will calculate linear path targets
+      double x_array[INTERP_INTVLS];
+      double y_array[INTERP_INTVLS];
+      double z_array[INTERP_INTVLS];
+      double hand_array[INTERP_INTVLS];
+
+      interpolate(current_x, x, x_array, ease_type);
+      interpolate(current_y, y, y_array, ease_type);
+      interpolate(current_z, z, z_array, ease_type);
+      interpolate(cur_hand, hand_angle, hand_array, ease_type);
+
+      for (byte i = 0; i < INTERP_INTVLS; i++)
+      {
+        double rot, left, right;
+        getCalAngles(x_array[i], y_array[i], z_array[i], rot, left, right);
+        writeAngle(rot, left, right, hand_array[i]);
+        delay(time * 1000 / INTERP_INTVLS);
+      }
+    }
+  }
+
+  // set final target position at end of interpolation or "atOnce"
+  writeAngle(tgt_rot, tgt_left, tgt_right, hand_angle);
+}
+
+void uArmClass::drawRec(double length_1, double length_2, double time_spend_per_length)
+{
+  moveTo(length_1,0,0,1,time_spend_per_length);
+  moveTo(0,length_2,0,1,time_spend_per_length);
+  moveTo(-length_1,0,0,1,time_spend_per_length);
+  moveTo(0,-length_2,0,1,time_spend_per_length);
+}
+
+void uArmClass::drawCur(double length_1, double length_2, int angle, double time_spend)
+{
+  uarm.attachAll();
+  double l_xp;
+  double l_yp;
+
+  calXYZ();
+  double current_x = g_cal_x;
+  double current_y = g_cal_y;
+  double current_z = g_cal_z;
+  double interp_arr[INTERP_INTVLS];
+
+  interpolate(0, angle/MATH_TRANS, interp_arr, INTERP_EASE_INOUT_CUBIC); 
+
+  for (byte i = 0; i < INTERP_INTVLS; i++){
+
+    l_xp = length_1 * cos(interp_arr[i]);
+    l_yp = length_2 * sin(interp_arr[i]);
+
+    calAngles( l_xp + current_x - length_1 , l_yp+ current_y , current_z);
+    uarm.writeAngle(g_theta_1, g_theta_2, g_theta_3,0);
+
+    delay(time_spend*1000/INTERP_INTVLS);
+  
+  }
+
+}
+
+double uArmClass::calYonly(double theta_1, double theta_2, double theta_3)
+{
+    //g_l3_1_2 = MATH_L3 * cos(theta_2 / MATH_TRANS);
+    //g_l4_1_2 = MATH_L4*cos(theta_3 / MATH_TRANS);
+    double l5_2 = (MATH_L2 + MATH_L3*cos(theta_2 / MATH_TRANS) + MATH_L4*cos(theta_3 / MATH_TRANS));
+
+    return -sin(abs(theta_1 / MATH_TRANS))*l5_2;
+}
+
+void uArmClass::gripperCatch()
+{
+  g_servo_hand.attach(SERVO_HAND);
+  g_servo_hand.write(HAND_ANGLE_CLOSE);
+  digitalWrite(VALVE_EN, LOW); // valve disable
+  digitalWrite(PUMP_EN, HIGH); // pump enable
+  g_gripper_reset = true;
+}
+
+void uArmClass::gripperRelease()
+{
+  if(g_gripper_reset)
+  {
+    g_servo_hand.attach(SERVO_HAND);
+    g_servo_hand.write(HAND_ANGLE_OPEN);
+    digitalWrite(VALVE_EN, HIGH); // valve enable, decompression
+    digitalWrite(PUMP_EN, LOW);   // pump disable
+    g_gripper_reset= false;
+  }
+}
+
+/* Action Control */
+void uArmClass::pumpOn()
+{
+
+   pinMode(PUMP_EN, OUTPUT);
+   pinMode(VALVE_EN, OUTPUT);
+   digitalWrite(VALVE_EN, LOW);
+   digitalWrite(PUMP_EN, HIGH);
+}
+
+void uArmClass::pumpOff()
+{
+   pinMode(PUMP_EN, OUTPUT);
+   pinMode(VALVE_EN, OUTPUT);
+   digitalWrite(VALVE_EN, HIGH);
+   digitalWrite(PUMP_EN, LOW);
+   delay(50);
+   digitalWrite(VALVE_EN,LOW);
+}
